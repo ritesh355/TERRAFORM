8f8474feb11225aea87b457831a20ced266cbc51
ritesh355 <riteshsingh86991@gmail.com>
2025-09-25 21:30:17 +0530

    name the file

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "my_bucket" {
   }
 }
 
-# Optional: Enable Versioning
+#  Enable Versioning
 resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.my_bucket.id # you also use .bucket in place od .id .id and .bucket often give the same result for S3, but .id is a Terraform standard practice for referencing a resource's unique identifier.
 
@@ -22,5 +22,12 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
   }
 }
 
+#  Block Public Access
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.my_bucket.id
 
-
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
--- day3-s3/s3-object/website/script.js
+++
@@ -1,2 +0,0 @@
-console.log("Website loaded successfully from S3!");
-
